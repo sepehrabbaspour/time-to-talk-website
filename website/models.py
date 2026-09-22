ac39c4436a0102c092a0fb9f3d771ce7537e8119
Sepehr <sepehr.abbaspour.1382@gmail.com>
2026-09-23 00:31:55 +0330

    Create database  and contact model
    create database by sqlite3
    create contact models and migrate to database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    company = models.CharField(max_length=255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
