2a029c0398616cc5d1f4c8add53c6149f2cbfcab
Sepehr <sepehr.abbaspour.1382@gmail.com>
2026-09-23 11:55:30 +0330

    Create form for contact page
    Create form for contact page with using model form
    
    Inquiry in views.py in website app
    
    Using the message to determine the success or failure of the message delivery in website\views.py and in template\website\contact.html

--- website/views.py
+++ website/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-
+from website.forms import ContactForm
+from django.contrib import messages
 # Create your views here.
 
 def index_view(request):
     return render(request , 'website/index.html')
 
 def contact_view(request):
-    return render(request , 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request , messages.SUCCESS , 'your ticket submited successfully')
+        else:
+            messages.add_message(request , messages.ERROR , 'your ticket didnt submited')
+    form = ContactForm()
+    return render(request , 'website/contact.html' , {'form' : form})
 
 def about_view(request):
     return render(request , 'website/about.html')
